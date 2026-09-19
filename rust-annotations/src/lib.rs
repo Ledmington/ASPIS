9529c4c677eec9d7efce1faa8082beafe881b80b
Ledmington <ledmington.dev@gmail.com>
2026-09-19 12:56:41 +0200

    Added new rust-annotations proc-macro crate

---
+++ rust-annotations/src/lib.rs
@@ -0,0 +1,20 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Hello)]
+pub fn hello(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let expanded = quote! {
+        impl #name {
+            pub fn hello(&self) {
+                println!("Hello from {}!", stringify!(#name));
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
